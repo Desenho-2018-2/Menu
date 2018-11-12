811af308610acaced02df3d1cf70d82035ffeddf
filypsdias <lipao.dias@hotmail.com>
2018-11-12 21:01:10 -0200

    Adding app's Models
    Co-authored-by: filypsdias <lipao.dias@hotmail.com>
    Co-authored-by: pAmanda <amandoca_779@hotmail.com>

--- menu/catalog/models.py
+++ menu/catalog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductType(models.Model):
+    """
+    This model represents a Product Type
+    """
+
+    # ProductType Primary Key
+    # idProductType = models.IntegerField() # Let's see Django create PK
+
+    # Product Description
+    description = models.TextField(max_length="100")
+
+
+class Product(models.Model):
+    """
+    This model represents a restaurant product 
+    """
+
+    # Product Primay Key
+    # idProduct = models.IntegerField() # Let's see Django create PK
+
+    # Product Type Foreign Key
+    productType = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+
+    # Product Description
+    description = models.TextField(max_length="100") 
+
+    # Product Name
+    name = models.TextField(max_length="20")
+
+    # Product Price
+    price = models.DecimalField(decimal_places=2, max_digits=3)
+    
+
+class Menu(models.Model):
+    """
+    This model represents a restaurant menu 
+    """
+
+    # product_id from extern service
+    product_id = models.IntegerField()
